ace7cfee144bc96613b7131a9f7cba462279e21a
meanzza123 <33210135+meanzza123@users.noreply.github.com>
2020-04-02 14:05:18 +0700

    fixtable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { setTheme } from 'ngx-bootstrap/utils';
 import { Location } from '@angular/common';
 import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,7 +11,7 @@ import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
 
 export class AppComponent {
   public dpConfig: Partial<BsDatepickerConfig> = new BsDatepickerConfig();
-  constructor(private location: Location) {
+  constructor(private location: Location,public router: Router) {
 
     setTheme('bs4');
   }
